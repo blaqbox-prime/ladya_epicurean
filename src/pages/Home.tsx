0e68e0e28f1f5d583958d1234b4dd5d4fe1f33c1
Blaqbox-prime <karabosambo@outlook.com>
2023-04-04 14:44:32 +0200

    Feature Section Aded

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -607,6 +607,70 @@ function Home() {
             </div>
           </div>
         </section>
+
+        {/* Features */}
+
+        <section className="features text-center">
+          <div className="container">
+            <p className="section-subtitle label-2">Why choose us</p>
+
+              <h2 className="headline-1 section-title">Our Strength</h2>
+
+              <GridList>
+                <li className="feature-item">
+                  <div className="feature-card">
+                    <div className="card-icon">
+                      <img src="assets/images/features-icon-1.png" width={100} height={80} loading="lazy" alt="icon" />
+                    </div>
+
+                    <h3 className="title-2 card-title">Hygienic Food</h3>
+                    <p className="labal-1 card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, necessitatibus.</p>
+                  </div>
+                </li>
+
+                <li className="feature-item">
+                  <div className="feature-card">
+                    <div className="card-icon">
+                      <img src="assets/images/features-icon-2.png" width={100} height={80} loading="lazy" alt="icon" />
+                    </div>
+
+                    <h3 className="title-2 card-title">Casual Environment</h3>
+                    <p className="labal-1 card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, necessitatibus.</p>
+                  </div>
+                </li>
+
+                <li className="feature-item">
+                  <div className="feature-card">
+                    <div className="card-icon">
+                      <img src="assets/images/features-icon-3.png" width={100} height={80} loading="lazy" alt="icon" />
+                    </div>
+
+                    <h3 className="title-2 card-title">Skilled Chef</h3>
+                    <p className="labal-1 card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, necessitatibus.</p>
+                  </div>
+                </li>
+
+                <li className="feature-item">
+                  <div className="feature-card">
+                    <div className="card-icon">
+                      <img src="assets/images/features-icon-4.png" width={100} height={80} loading="lazy" alt="icon" />
+                    </div>
+
+                    <h3 className="title-2 card-title">Event & Party</h3>
+                    <p className="labal-1 card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, necessitatibus.</p>
+                  </div>
+                </li>
+
+              </GridList>
+
+              <img src="assets/images/shape-1.png" width={208} height={178} loading="lazy" alt="shape" className="shape shape-1" />
+
+              <img src="assets/images/shape-8.png" width={120} height={115} loading="lazy" alt="shape" className="shape shape-2" />
+
+
+          </div>
+        </section>
+
       </article>
     </main>
   );
